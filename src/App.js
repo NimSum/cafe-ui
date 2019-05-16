94724f47d3b24cc84a4ecb314d5235ff12d2f744
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-16 10:16:26 -0600

    Create cancel reservation method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { fetchReservations, postNewReservation } from './fetches';
+import { fetchReservations, postNewReservation, removeReservation } from './fetches';
 import ReservationsContainer from './ReservationsContainer';
 import ReservationForm from './ReservationForm';
 
@@ -21,6 +21,11 @@ class App extends Component {
     const result = await postNewReservation(reservation);
     const updatedReservations = [ ...this.state.reservations, result ]
     await this.setState({ reservations: updatedReservations });
+  }
+
+  cancelReservation = async (reservationId) => {
+    const result = await removeReservation(reservationId);
+    await this.setState({ reservations: result });
   }
 
   render() {
